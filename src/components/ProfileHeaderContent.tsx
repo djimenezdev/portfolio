ea5b8ad439a945053623c37503b199c0ae936e74
Daniel Jimenez <daniel@djimenezdev.com>
2024-08-24 04:16:53 -0700

    FIX: moved header content to own comp

---
+++ src/components/ProfileHeaderContent.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const ProfileHeaderContent = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold tracking-tight text-black dark:text-slate-200 sm:text-6xl">
+        <Link href="/">Daniel Jimenez</Link>
+      </h1>
+      <h2 className="mt-2 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+        Full-Stack Developer
+      </h2>
+      <p className="mt-3 text-lg text-slate-300 max-w-xs leading-normal">
+        Crafting beautiful and well-built Web applications with passion
+      </p>
+    </>
+  );
+};
+export default ProfileHeaderContent;
